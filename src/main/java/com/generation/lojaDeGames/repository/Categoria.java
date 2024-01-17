eeca2f1a063b590685590b470a7236e85f27e052
Daniela Xavier <danielamxsoares@gmail.com>
2024-01-17 13:24:12 -0300

    fix: incluir anotação

--- src/main/java/com/generation/lojaDeGames/repository/Categoria.java
+++ src/main/java/com/generation/lojaDeGames/repository/Categoria.java
@@ -1,7 +1,9 @@
 package com.generation.lojaDeGames.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface Categoria extends JpaRepository<Categoria, Long>{
 
 }
--- src/main/java/com/generation/lojaDeGames/repository/Produto.java
+++ src/main/java/com/generation/lojaDeGames/repository/Produto.java
@@ -1,7 +1,9 @@
 package com.generation.lojaDeGames.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface Produto extends JpaRepository<Produto, Long>{
 
 }
