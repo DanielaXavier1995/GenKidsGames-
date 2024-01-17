bea62f6fb26a12b00ff8d99c045b86fa7bb5bb21
Daniela Xavier <danielamxsoares@gmail.com>
2024-01-17 15:35:03 -0300

    feat: implementar método de consulta personalizada

--- src/main/java/com/generation/lojaDeGames/repository/CategoriaRepository.java
+++ src/main/java/com/generation/lojaDeGames/repository/CategoriaRepository.java
@@ -1,6 +1,9 @@
 package com.generation.lojaDeGames.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.lojaDeGames.model.Categoria;
@@ -8,4 +11,5 @@ import com.generation.lojaDeGames.model.Categoria;
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 
+	public List<Categoria> findAllByTemaCategoriaContainingIgnoreCase(@Param("temaCategoria") String tema);
 }
