00515433fd9da4f924d59bc685d990db58170e1f
Daniela Xavier <danielamxsoares@gmail.com>
2024-01-17 13:21:29 -0300

    feat: criar camada repository para produto e categoria

---
+++ src/main/java/com/generation/lojaDeGames/repository/Categoria.java
@@ -0,0 +1,7 @@
+package com.generation.lojaDeGames.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Categoria extends JpaRepository<Categoria, Long>{
+
+}
---
+++ src/main/java/com/generation/lojaDeGames/repository/Produto.java
@@ -0,0 +1,7 @@
+package com.generation.lojaDeGames.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Produto extends JpaRepository<Produto, Long>{
+
+}
